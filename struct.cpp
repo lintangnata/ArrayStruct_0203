a3dae8a23302a6c63d6034f8ad2288de6b37329d
LintangNata <lintangnata4@gmail.com>
2025-04-14 15:48:47 +0700

    finish struck

--- struct.cpp
+++ struct.cpp
@@ -4,5 +4,25 @@ using namespace std;
 struct Hero{
     string Nama;
     string Role;
-}
+    string Tipe;
+};
 
+int main()
+{
+    Hero arhero;
+
+    cout << "Masukkan Nama Hero = ";
+    cin >> arhero.Nama;
+    cout << "Masukkan Role Hero = ";
+    cin >> arhero.Role;
+    cout << "Masukkan Tipe Hero = ";
+    cin >> arhero.Tipe;
+
+    cout << endl;
+    cout << "Tampilkan Hero = " << endl;
+    cout << endl;
+
+    cout << "Nama Hero = " << arhero.Nama << endl;
+    cout << "Role Hero = " << arhero.Role << endl;
+    cout << "Tipe Hero = " << arhero.Tipe << endl;
+}
